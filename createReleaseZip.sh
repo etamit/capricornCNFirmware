cd158212f50214b6c56bae6d4e9618f34a100e7e
Unknown <uvoea@student.kit.edu>
2018-12-06 17:26:28 +0100

    variable github user

--- createReleaseZip.sh
+++ createReleaseZip.sh
@@ -1,15 +1,17 @@
-#! /bin/bash
+#!/bin/bash
 # Create official Release on GitHub.
  
 # config
-ACCESSTOKEN="0123456789"
-GITUSER="username"
+echo Enter GitHub access token:
+read ACCESSTOKEN
+echo Enter GitHub user name:
+read GITUSER
 NAME=${PWD##*/} # name of your GitHub repository
  
 GITPATH=`pwd` # this file should be in the base of your local git repository
 # Get version from readme
 NEWVERSION=`grep "^Version" "$GITPATH/README.md" | awk -F' ' '{print $2}' | sed 's/[[:space:]]//g'`
- 
+
 # Let's begin...
 echo ".........................................."
 echo
