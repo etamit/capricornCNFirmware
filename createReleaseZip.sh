ce513cfc07d0088c18a6179b06c8f070baeed804
Unknown <uvoea@student.kit.edu>
2018-12-06 17:24:55 +0100

    local automated build script
    copypasta https://isabelcastillo.com/script-release-github

---
+++ createReleaseZip.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# Create official Release on GitHub.
+ 
+# config
+ACCESSTOKEN="0123456789"
+GITUSER="username"
+NAME=${PWD##*/} # name of your GitHub repository
+ 
+GITPATH=`pwd` # this file should be in the base of your local git repository
+# Get version from readme
+NEWVERSION=`grep "^Version" "$GITPATH/README.md" | awk -F' ' '{print $2}' | sed 's/[[:space:]]//g'`
+ 
+# Let's begin...
+echo ".........................................."
+echo
+echo "Preparing to release version $NEWVERSION on GitHub"
+echo
+echo ".........................................."
+echo
+ 
+# Create a Release on GitHub
+cd "$GITPATH"
+echo "Creating a new release on GitHub"
+API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $NEWVERSION $NEWVERSION $NEWVERSION)
+curl --data "$API_JSON" https://api.github.com/repos/${GITUSER}/${NAME}/releases?access_token=${ACCESSTOKEN}
+ 
+echo "*** FIN ***"
