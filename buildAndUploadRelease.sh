fa7f9382465197df40214568f792c8f4547acf9f
Unknown <uvoea@student.kit.edu>
2019-01-24 19:25:28 +0100

    Case handling: access token, zip, tag

--- buildAndUploadRelease.sh
+++ buildAndUploadRelease.sh
@@ -15,30 +15,64 @@ read tag
 repoName=${PWD##*/} # name of your GitHub repository
 assetFileName=${repoName}"-"$tag.zip
 
- 
-localRepoPath=`pwd` # this file should be in the base of your local git repository
-# Let's begin...
+#check if in a repo(?)
+#localRepoPath=`pwd` # this file should be in the base of your local git repository
+
+#Test for valid Access Token
+accessTokenStatus=$(curl -o -I -s -w "%{http_code}" -H "Authorization: token $accessToken" https://api.github.com)
+if [ $accessTokenStatus -eq 200 ]; then
+	echo "Access token is valid ($accessTokenStatus)"
+elif [ $accessTokenStatus == 401 ];then
+	echo "Access token is invalid ($accessTokenStatus)"
+	echo abort
+	exit
+else 
+	echo "Access token unkown status ($accessTokenStatus)"
+	echo abort
+	exit
+fi
+
+
+
 echo
 echo ".........................................."
 echo "1/3: Building Release Asset "$assetFileName
 echo ".........................................."
 echo
 zip -r $assetFileName firmware-update META-INF
-echo
-echo " done 1/3"
 
-# Create a Release on GitHub
+#Test zip exit code
+zipExitCode=$?
+if [ "$zipExitCode" != 0 ]; then
+	echo "zip exit code zipExitCode"
+	echo abort
+	exit
+else
+	echo "Release file zipped successfully"
+fi
+
+
+
 echo
 echo ".........................................."
 echo "2/3: Creating Release "$tag
 echo ".........................................."
 echo
-cd "$localRepoPath"
+#cd "$localRepoPath"
 jsonCreateRelease=$(printf '{"tag_name": "'$tag'","target_commitish": "master","name": "'$tag'","draft": false,"prerelease": false}' $tag $tag $tag)
 id=$(curl --data "$jsonCreateRelease" https://api.github.com/repos/${gitUser}/${repoName}/releases?access_token=${accessToken} | grep -m 1 "id.:" | tr -cd [:digit:])
-echo
-echo "Release ID is " $id
-echo " done 2/3"
+
+#Test for plausible id
+if [ "$id" == "" ]; then
+	echo "No id for new release tag received (unsuccessful?)"
+	echo abort
+	exit
+else
+	echo "Id for release tag is $id"
+fi
+
+
+
 echo
 echo ".........................................."
 echo "3/3: Adding Release Asset"
@@ -46,5 +80,6 @@ echo ".........................................."
 echo
 upload_url="https://uploads.github.com/repos/$gitUser/$repoName/releases/$id/assets?name=$(basename $assetFileName)"
 curl -o /dev/null --data-binary @"$assetFileName" -H "Authorization: token $accessToken" -H "Content-Type: application/octet-stream" $upload_url
+#TODO upload successful?
 echo
 echo " done 3/3"
